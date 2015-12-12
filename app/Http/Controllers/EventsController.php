b49f6ebabbaf242901bd43c3547d9b67b72bb295
Andrew Kramer <ajkramer12@gmail.com>
2015-12-11 19:04:38 -0500

    Finished edit event functionality

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -110,35 +110,130 @@ class EventsController extends Controller {
     * Edit Location /
     */
     public function editEvent($timeline_id, $event_id, Request $request) {
-        $timeline = \App\Timeline::where('id', '=', $timeline_id)
-			->first();
-			
+		// Try to get event
 		$event = \App\Event::where('id', '=', $event_id)
 			->first();
 		
+		// If event exists, continue
 		if ($event) {
 			
+			$timeline = \App\Timeline::where('id', '=', $timeline_id)
+				->first();
+			
+			$event_characters = array();
+			foreach($event->characters as $character) {
+				$event_characters[] = $character;
+			}
+			
+			$event_locations = array();
+			foreach($event->locations as $location) {
+				$event_locations[] = $location;
+			}
+			
+			$characters = \App\Character::where('timeline_id', '=', $timeline_id)
+				->orderBy('name')
+				->get(); 
+				
+			$locations = \App\Location::where('timeline_id', '=', $timeline_id)
+				->orderBy('name')
+				->get(); 
+			
+			// If form has not been submitted, show form
 			if ($request -> input('showForm') == 'true') {
 				return view('events.editEvent')
 					->with('showForm', 'true')
 					->with('event', $event)
+					->with('event_characters', $event_characters)
+					->with('event_locations', $event_locations)
+					->with('characters', $characters)
+					->with('locations', $locations)
 					->with('timeline', $timeline);
-			} else {	
+			} else { // Else submit form	
 				
 					$event->name = $request -> input('name');
 					$event->start_date = $request -> input('start_date');
 					$event->end_date = $request -> input('end_date');
 					$event->description = $request -> input('description');
-
+					
 					$event->save();
+					
+					// Edit existing locations
+					for ($l = 0; $l < count($event_locations); $l++) {
+						
+						$location = \DB::table('event_location')
+							->where('location_id', '=', $request -> input('location_id'.$l))
+							->where('event_id', '=', $event_id)
+							->first();
+						
+						if($location) {
+							if($request -> input('location'.$l) == '--Delete Location--') {
+								\DB::table('event_location')
+									->where('location_id', '=', $request -> input('location_id'.$l))
+									->where('event_id', '=', $event_id)
+									->delete();
+							} else { 
+								$location_name = \App\Location::where('name', '=', $request -> input('location'.$l))
+									->first();
+							
+								\DB::table('event_location')
+									->where('location_id', '=', $request -> input('location_id'.$l))
+									->where('event_id', '=', $event_id)
+									->update(['location_id' => $location_name->id]);
+							}
+						}
+					}
+					
+					// Add new location to database
+					$location_input = $request -> input('location_new');
+					if ($location_input != 'Choose a Location') {
+							$location = \App\Location::where('name','LIKE',$location_input)->first();
+							$event->locations()->save($location);
+					}
+					
+					
+					
+					
+					// Edit existing characters
+					for ($c = 0; $c < count($event_characters); $c++) {
+						
+						$character = \DB::table('character_event')
+							->where('character_id', '=', $request -> input('character_id'.$c))
+							->where('event_id', '=', $event_id)
+							->first();
+						
+						if($character) {
+							if($request -> input('character'.$c) == '--Delete Character--') {
+								\DB::table('character_event')
+									->where('character_id', '=', $request -> input('character_id'.$c))
+									->where('event_id', '=', $event_id)
+									->delete();
+							} else { 
+								$character_name = \App\Character::where('name', '=', $request -> input('character'.$c))
+									->first();
+							
+								\DB::table('character_event')
+									->where('character_id', '=', $request -> input('character_id'.$c))
+									->where('event_id', '=', $event_id)
+									->update(['character_id' => $character_name->id]);
+							}
+						}
+					}
+					
+					// Add new character to database
+					$character_input = $request -> input('character_new');
+					if ($character_input != 'Choose a Character') {
+							$character = \App\Character::where('name','LIKE',$character_input)->first();
+							$event->characters()->save($character);
+					}
 
+					// Return success message
 					return view('events.editEvent')
 						->with('showForm', 'false')
 						->with('event', $event)
 						->with('timeline', $timeline);
 				
 			}
-		} else {
+		} else { // Else fail
 			return "Update failed. Event does not exist.";
 		}
     }
--- resources/views/events/editEvent.blade.php
+++ resources/views/events/editEvent.blade.php
@@ -19,10 +19,58 @@
 				<label for="end_date">End Date</label>
 				<input id="end_date" class="form-control" type="text" name="end_date" value="{{ $event->end_date }}" >
 			</div>
+			
+			<p>
+				Location: 
+				@for ($l = 0; $l < count($event_locations); $l++)
+					<input id="location_id{{ $l }}" type="hidden" name="location_id{{ $l }}" value="{{ $event_locations[$l]->id }}">
+					<select id="location{{ $l }}" class="form-control" name="location{{ $l }}">
+						<option>{{ $event_locations[$l]->name }}</option>
+						@foreach ($locations as $location_list)
+							<option>{{ $location_list->name }}</option>
+						@endforeach
+						<option>--Delete Location--</option>
+					</select>
+				@endfor
+			</p>
+			<div class="form-group">
+				<label for="location_new">Add a Location</label>
+				<select id="location_new" class="form-control" name="location_new">
+					<option>Choose a Location</option>
+					@foreach ($locations as $location)
+						<option>{{ $location->name }}</option>
+					@endforeach
+				</select>
+			</div>
+			
 			<div class="form-group">
 				<label for="description">Description</label>
 				<textarea id="description" class="form-control" name="description" rows="4">{{ $event->description }}</textarea>
 			</div>
+			
+			<h5>Characters:</h5>
+			<ul>
+				@for ($c = 0; $c < count($event_characters); $c++)
+					<input id="character_id{{ $c }}" type="hidden" name="character_id{{ $c }}" value="{{ $event_characters[$c]->id }}">
+					<select id="character{{ $c }}" class="form-control" name="character{{ $c }}">
+						<option>{{ $event_characters[$c]->name }}</option>
+						@foreach ($characters as $character_list)
+							<option>{{ $character_list->name }}</option>
+						@endforeach
+						<option>--Delete Character--</option>
+					</select>
+				@endfor
+			</ul>
+			<div class="form-group">
+				<label for="character_new">Add a Character</label>
+				<select id="character_new" class="form-control" name="character_new">
+					<option>Choose a Character</option>
+					@foreach ($characters as $character)
+						<option>{{ $character->name }}</option>
+					@endforeach
+				</select>
+			</div>
+			
 			<button class="btn btn-primary">Submit</button>
 		</form>
 	@else
